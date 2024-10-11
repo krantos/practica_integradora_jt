c85c34f25a179a5e0b927cbdfd0049c3f6b2209a
krantos <marcosriverosk@gmail.com>
2024-10-10 22:18:15 -0300

    Tenant step 1 add tenat to the rest of the models

---
+++ database/migrations/2024_10_10_021038_add_tenant_to_the_rest_of_tables.php
@@ -0,0 +1,77 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+          $table->unsignedBigInteger('tenant_id')->nullable()->index();
+        });
+				
+				Schema::table('offers', function (Blueprint $table) {
+					$table->unsignedBigInteger('tenant_id')->nullable()->index();
+				});
+				
+				Schema::table('locations', function (Blueprint $table) {
+					$table->unsignedBigInteger('tenant_id')->nullable()->index();
+				});
+				
+				Schema::table('companies', function (Blueprint $table) {
+					$table->unsignedBigInteger('tenant_id')->nullable()->index();
+				});
+
+				Schema::table('platforms', function (Blueprint $table) {
+					$table->unsignedBigInteger('tenant_id')->nullable()->index();
+				});
+
+				Schema::table('interviews', function (Blueprint $table) {
+					$table->unsignedBigInteger('tenant_id')->nullable()->index();
+				});
+
+				Schema::table('offers_technologies', function (Blueprint $table) {
+					$table->unsignedBigInteger('tenant_id')->nullable()->index();
+				});
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+			// Schema::table('users', function (Blueprint $table) {
+			// 	$table->dropIndex('tenant_id');
+			// });
+			
+			// Schema::table('offers', function (Blueprint $table) {
+			// 	$table->dropIndex('tenant_id');
+			// });
+			
+			// Schema::table('locations', function (Blueprint $table) {
+			// 	$table->dropIndex('tenant_id');
+			// });
+			
+			// Schema::table('companies', function (Blueprint $table) {
+			// 	$table->dropIndex('tenant_id');
+			// });
+
+			// Schema::table('platforms', function (Blueprint $table) {
+			// 	$table->dropIndex('tenant_id');
+			// });
+
+			// Schema::table('interviews', function (Blueprint $table) {
+			// 	$table->dropIndex('tenant_id');
+
+			// });
+
+			// Schema::table('offers_technologies', function (Blueprint $table) {
+			// 	$table->dropIndex('tenant_id');
+			// });
+    }
+};
